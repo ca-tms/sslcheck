99a25cf37a0a1a231dc051b1c18765983b60c714
Fabian Letzkus <letzkus@donotconnect.de>
2014-02-16 20:11:13 +0100

    Added Comment and toString Method for SSLInfo

--- src/sslcheck/system/SSLInfo.java
+++ src/sslcheck/system/SSLInfo.java
@@ -7,6 +7,14 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 
+/**
+ * SSLInfo is a class used for storing HTTPS/SSL-related information such as URL of the website
+ * or the certificates given by the server during the SSL Session. It does not provide any real
+ * functionality.
+ * 
+ * @author letzkus
+ *
+ */
 public class SSLInfo {
 
 	URL url;
@@ -37,12 +45,36 @@ public class SSLInfo {
 	 *      https://stackoverflow.com/questions/1270703/how-to-retrieve-compute-an
 	 *      -x509-certificates-thumbprint-in-java
 	 */
-	public String getThumbPrint(X509Certificate cert)
+	public String getThumbPrint(Certificate cert)
 			throws NoSuchAlgorithmException, CertificateEncodingException {
 		MessageDigest md = MessageDigest.getInstance("SHA-1");
 		byte[] der = cert.getEncoded();
 		md.update(der);
 		return md.digest().toString();
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		String str ="";
+		str.concat("URL: "+this.url+"\n");
+		for(Certificate cer : this.certs) {
+			if(cer==null) { // do we really need this?
+				str.concat("------- NULL Certificate found -------");
+				continue;
+			}
+			str.concat("----------------------------------------------------");
+			str.concat(cer.toString()+"\n");
+			try {
+				str.concat("SHA-1 Hash: "+this.getThumbPrint(cer));
+			} catch (Exception e) {
+				str.concat("SHA-1 Hash not available: "+e);
+			}
+			str.concat("----------------------------------------------------");
+		}
+		return str;
 	}
 
 
