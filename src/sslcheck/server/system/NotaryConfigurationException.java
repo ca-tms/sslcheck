6f0d67ffbc65c79fd0c51b1b1f6e4ef88050c91c
Fabian Letzkus <letzkus@donotconnect.de>
2014-02-16 19:36:13 +0100

    Added NotaryConfigurationException. ErrorHandling is now done in NotaryManager.

---
+++ src/sslcheck/server/system/NotaryConfigurationException.java
@@ -0,0 +1,14 @@
+package sslcheck.server.system;
+
+public class NotaryConfigurationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2432290993021963868L;
+
+	public NotaryConfigurationException(String msg) {
+		super(msg);
+	}
+
+}
