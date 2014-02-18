6933c9de65a6b9c42b82140abc1e720f9c1cf8fa
Fabian Letzkus <letzkus@donotconnect.de>
2014-02-18 23:17:45 +0100

    - Correct Implementation of singleton - Correct loading of properties - Added log4j messages

--- src/sslcheck/core/NotaryConfiguration.java
+++ src/sslcheck/core/NotaryConfiguration.java
@@ -1,8 +1,7 @@
 package sslcheck.core;
 
-import java.io.FileInputStream;
-import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -13,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 
 public class NotaryConfiguration {
 	
-	private final static Logger log = LogManager.getLogger("NotaryConfiguration");
+	private final static Logger log = LogManager.getLogger("core.NotaryConfiguration");
 	
 	Properties param = null;
 	
@@ -23,44 +22,49 @@ public class NotaryConfiguration {
 	public static NotaryConfiguration getInstance() { // NotarConfiguration is a singleton!!
 		if(instance == null)
 			try {
-				return new NotaryConfiguration();
+				instance = new NotaryConfiguration();
 			} catch (IOException e) {
-				e.printStackTrace();
+				log.error("Error instanciating NotaryConfiguration(): "+e);
 			}
 		return instance;
 	}
 	
 	public NotaryConfiguration() throws IOException {
 		this.param = new Properties();
-		BufferedInputStream stream = new BufferedInputStream(new FileInputStream("notaries.properties"));
-		this.param.load(stream);
-		stream.close();
+		InputStream istream = this.getClass()
+		        .getResourceAsStream("/notaries.properties");
+		this.param.load(istream);
+		istream.close();
 		
-		Enumeration<Object> params = this.param.elements();
+		Enumeration<Object> params = this.param.keys();
+		log.debug("Size of notaries.properties file: "+this.param.size());
 		
 		while(params.hasMoreElements()) {
 			String p = ((String) params.nextElement()); // Please surround me with try-catch..
+			log.debug("Parsing config value... "+p);
 			int idx = p.indexOf(".");
-			String p_name = p.substring(0,idx);
-			String p_confVariable = p.substring(idx+1, p.length()-1);
+			if(idx!=-1) {
+				String p_name = p.substring(0,idx);
+				String p_confVariable = p.substring(idx+1, p.length()-1);
 			
-			if(!this.notaryConfs.containsKey(p_name)){
-				ArrayList<String> variables = new ArrayList<String>();
-				variables.add(p_confVariable);
-				notaryConfs.put(p_name,variables);	
-			}else{
-				ArrayList<String> variables = this.notaryConfs.get(p_name);
-				this.notaryConfs.remove(p_name);
-				variables.add(p_confVariable);
-				notaryConfs.put(p_name,variables);	
-			}	
+				if(!this.notaryConfs.containsKey(p_name)){
+					ArrayList<String> variables = new ArrayList<String>();
+					variables.add(p_confVariable);
+					notaryConfs.put(p_name,variables);	
+				}else{
+					ArrayList<String> variables = this.notaryConfs.get(p_name);
+					this.notaryConfs.remove(p_name);
+					variables.add(p_confVariable);
+					notaryConfs.put(p_name,variables);	
+				}	
+			}
 		}
 	}
 	
 	public String getValue(String key, String notary) throws NotaryConfigurationException { 
 		String ret = this.param.getProperty(notary + "." + key);
 		if(ret==null) {
-			log.error("Property \""+notary+"."+key+"\" not found, trying DefaultNotary."+key+".");
+			log.warn("Property "+notary+"."+key+" not found, trying DefaultNotary."+key+".");
 			return getValue(key);
 		}
 		return ret;
@@ -69,7 +73,7 @@ public class NotaryConfiguration {
 	public String getValue(String key) throws NotaryConfigurationException {
 		String ret = this.param.getProperty("DefaultNotary." + key);
 		if(ret==null) {
-			log.error("Property \"DefaultNotary."+key+"\" not found.");
+			log.error("Property DefaultNotary."+key+" not found.");
 			throw new NotaryConfigurationException("DefaultNotary."+key+" not found.");
 		}
 		return ret;
