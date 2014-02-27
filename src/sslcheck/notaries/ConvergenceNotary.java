37f277825ba159d6bd4f2eb2d0a9f29be97366ea
Fabian Letzkus <letzkus@donotconnect.de>
2014-02-27 18:04:09 +0100

    Added function to convert certificate hash to Convergence compatible format.

--- src/sslcheck/notaries/ConvergenceNotary.java
+++ src/sslcheck/notaries/ConvergenceNotary.java
@@ -13,11 +13,32 @@ public class ConvergenceNotary extends Notary {
 	@Override
 	public float check(String h, X509Certificate c) {
 		// First Phase, just print the certificate to check
-		log.info("Checking Host: " + h);
-		log.info("Checking Certificate " + c.getSHA1Fingerprint());
-
+		log.trace("-- BEGIN -- ConvergenceNotary.check() ");
+		String convergenceCompatibleHash = this.convertHash(c
+				.getSHA1Fingerprint());
+		log.info("Checking Certificate for " + h
+				+ " using convergenceCompatibleHash "
+				+ convergenceCompatibleHash);
+		log.trace("-- DONE -- ConvergenceNotary.check() ");
 		// Note to myself: What todo if getPresumedHost() == null -> Certificate
 		// is CA Certificate...
 		return 100;
+	}
+
+	/**
+	 * Formats a standard hash a2b3b5... to the format A2:B3:B5...
+	 * 
+	 * @param h the Hash
+	 * @return
+	 */
+	private String convertHash(String h) {
+		String convergenceCompatibleHash = "";
+		h = h.toUpperCase();
+		for (int i = 0; i < h.length(); i++) {
+			convergenceCompatibleHash += h.charAt(i);
+			if (i % 2 == 1 && i != h.length() - 1)
+				convergenceCompatibleHash += ":";
+		}
+		return convergenceCompatibleHash;
 	}
 }
