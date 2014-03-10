abef285af48576fe3454819bdb2fa40fa1db0aab
Fabian Letzkus <letzkus@donotconnect.de>
2014-03-10 17:15:02 +0100

    see e2f2613f2a

---
+++ src/sslcheck/core/TLSCertificateException.java
@@ -0,0 +1,14 @@
+package sslcheck.core;
+
+public class TLSCertificateException extends Exception {
+
+	public TLSCertificateException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8873695872516898969L;
+
+}
