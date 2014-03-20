bf2ebe145ea1bcc3374a25ff046633eb35f4609c
Fabian Letzkus <letzkus@donotconnect.de>
2014-03-20 12:45:58 +0100

    Ups.. this was missing...

---
+++ src/sslcheck/notaries/NotaryException.java
@@ -0,0 +1,14 @@
+package sslcheck.notaries;
+
+public class NotaryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7179480163738909546L;
+
+	public NotaryException(String msg) {
+		super(msg);
+	}
+
+}
